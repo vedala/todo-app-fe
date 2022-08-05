6fc9b2ad4e82901fa4eed01c25faa61d9b0d6c28
Kalyan Vedala <svedala23@gmail.com>
2022-08-05 10:28:28 -0700

    Capture click of menu items

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,11 +1,24 @@
 import React from "react"
 
 function Menu() {
+    const MENU_TABS = {
+        TO_DO: 'TO_DO',
+        DONE: 'DONE',
+        ALL: 'ALL'
+    }
+
+    const [selectedTab, setSelectedTab] = React.useState(MENU_TABS.TO_DO)
+
+    function handleClick(tabClicked) {
+        setSelectedTab(tabClicked)
+        console.log(tabClicked)
+    }
+
     return (
         <ul className="menu">
-            <li>TO DO</li>
-            <li>DONE</li>
-            <li>ALL</li>
+            <li onClick={() => handleClick(MENU_TABS.TO_DO)}>TO DO</li>
+            <li onClick={() => handleClick(MENU_TABS.DONE)}>DONE</li>
+            <li onClick={() => handleClick(MENU_TABS.ALL)}>ALL</li>
         </ul>
     )
 }
