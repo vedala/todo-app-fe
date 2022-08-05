6bf6a679db417157f71a525aacbc7877d95c2d71
Kalyan Vedala <svedala23@gmail.com>
2022-08-04 20:50:05 -0700

    Create tasks array state in App, add function to handle task addition

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
+import React from "react"
 import './App.css';
 import Header from "./components/Header"
 import TaskList from "./components/TaskList"
 
 function App() {
+  const [tasks, setTasks] = React.useState(["task1", "task2"])
+
+  function handleTasksChange(newTask) {
+    setTasks(prevState => {
+      return [...prevState, newTask]
+    })
+  }
+
   return (
     <div className="App">
-      <Header />
-      <TaskList />
+      <Header handleTasksChange={handleTasksChange} />
+      <TaskList tasks={tasks}/>
     </div>
   );
 }
