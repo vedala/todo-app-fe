f4a0c20a9501ed0cad8ce412e7600fefe3bdb6b2
Kalyan Vedala <svedala23@gmail.com>
2022-08-05 06:15:46 -0700

    Add Menu component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,13 @@
 import React from "react"
 import TaskInput from "./TaskInput"
+import Menu from "./Menu"
 
 function Header(props) {
     return (
         <div>
             <h1>To-Do List</h1>
             <TaskInput handleTaskCreate={props.handleTaskCreate}/>
+            <Menu />
         </div>
     )
 }
---
+++ src/components/Menu.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+function Menu() {
+    return (
+        <ul>
+            <li>TO DO</li>
+            <li>DONE</li>
+            <li>ALL</li>
+        </ul>
+    )
+}
+
+export default Menu
