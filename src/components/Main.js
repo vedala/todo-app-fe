44128fd6dd2e7f2954ce3d58d32f938f85f1ff14
Kalyan Vedala <svedala23@gmail.com>
2022-08-11 10:26:03 -0700

    Remove overlay markup

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,33 +5,28 @@ import DeleteConfirm from "./DeleteConfirm"
 function Main(props) {
     const [deletePrepStatus, setDeletePrepStatus] = React.useState(false)
 
-    const [displayOverlay, setDisplayOverlay] = React.useState(false)
-
     function handleDeletePrepClick() {
         setDeletePrepStatus(prevState => !prevState)
     }
 
     function handleDeleteConfirmClick() {
-        setDisplayOverlay(prevState => !prevState)
+        console.log("DeleteConfirmClick")
     }
 
     return (
         <div className="main">
-            <div className="main-content-wrapper">
-                {deletePrepStatus && <DeleteConfirm handleDeleteConfirmClick={handleDeleteConfirmClick}/>}
-                <TaskList
-                    tasks={props.tasks}
-                    MENU_TABS={props.MENU_TABS}
-                    selectedTab={props.selectedTab}
-                    deletePrepStatus={deletePrepStatus}
-                    handleDeletePrepClick={handleDeletePrepClick}
-                    updateTaskStatus={props.updateTaskStatus}
-                />
-                <button className="main-delete-prepare" onClick={handleDeletePrepClick}>
-                    {deletePrepStatus ? "Delete Cancel" : "Delete Prepare"}
-                </button>
-            </div>
-            {displayOverlay && <div className="delete-overlay">Overlay Window</div>}
+            {deletePrepStatus && <DeleteConfirm handleDeleteConfirmClick={handleDeleteConfirmClick}/>}
+            <TaskList
+                tasks={props.tasks}
+                MENU_TABS={props.MENU_TABS}
+                selectedTab={props.selectedTab}
+                deletePrepStatus={deletePrepStatus}
+                handleDeletePrepClick={handleDeletePrepClick}
+                updateTaskStatus={props.updateTaskStatus}
+            />
+            <button className="main--delete-prepare" onClick={handleDeletePrepClick}>
+                {deletePrepStatus ? "Delete Cancel" : "Delete Prepare"}
+            </button>
         </div>
     )
 }
