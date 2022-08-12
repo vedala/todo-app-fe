d19c39e106689dc33819a90e402e53d597f94fd2
Kalyan Vedala <svedala23@gmail.com>
2022-08-11 22:06:03 -0700

    Change indent to 4 spaces

--- src/App.js
+++ src/App.js
@@ -7,92 +7,92 @@ import Main from "./components/Main"
 
 function App() {
 
-  const MENU_TABS = {
-    TO_DO: 'TO_DO',
-    DONE: 'DONE',
-    ALL: 'ALL'
-  }
-
-  const [selectedTab, setSelectedTab] = React.useState(MENU_TABS.TO_DO)
-
-  function handleTabSelection(tabClicked) {
-      setSelectedTab(tabClicked)
-  }
-
-  const uid = new ShortUniqueId()
-
-  const [tasks, setTasks] = React.useState(
-    [
-      {
-        id: uid(),
-        text: "task1",
-        active: true
-      },
-      {
-        id: uid(),
-        text: "task2",
-        active: true
-      },
-      {
-        id: uid(),
-        text: "task3",
-        active: false
-      },
-      {
-        id: uid(),
-        text: "task4",
-        active: false
-      }
-    ]
-  )
-
-  function handleTaskCreate(newTaskText) {
-    const newTask = {
-      id: uid(),
-      text: newTaskText,
-      active: true
+    const MENU_TABS = {
+        TO_DO: 'TO_DO',
+        DONE: 'DONE',
+        ALL: 'ALL'
     }
 
-    setTasks(prevState => {
-      return [...prevState, newTask]
-    })
-  }
+    const [selectedTab, setSelectedTab] = React.useState(MENU_TABS.TO_DO)
 
-  function updateTaskStatus(taskId) {
-    setTasks(prevTasks => prevTasks.map(task => {
-        if (task.id === taskId) {
-          return {
-            ...task,
-            active: !task.active
-          }
-        } else {
-          return task
+    function handleTabSelection(tabClicked) {
+        setSelectedTab(tabClicked)
+    }
+
+    const uid = new ShortUniqueId()
+
+    const [tasks, setTasks] = React.useState(
+        [
+            {
+                id: uid(),
+                text: "task1",
+                active: true
+            },
+            {
+                id: uid(),
+                text: "task2",
+                active: true
+            },
+            {
+                id: uid(),
+                text: "task3",
+                active: false
+            },
+            {
+                id: uid(),
+                text: "task4",
+                active: false
+            }
+        ]
+    )
+
+    function handleTaskCreate(newTaskText) {
+        const newTask = {
+            id: uid(),
+            text: newTaskText,
+            active: true
         }
-    }))
-  }
 
-  function deleteTask(taskId) {
-    setTasks(prevTasks => prevTasks.filter(task => task.id !== taskId))
-  }
+        setTasks(prevState => {
+            return [...prevState, newTask]
+        })
+    }
 
-  return (
-    <div className="app">
-      <Header
-        selectedTab={selectedTab}
-        MENU_TABS={MENU_TABS}
-        tasks={tasks}
-        handleTaskCreate={handleTaskCreate}
-        handleTabSelection={handleTabSelection}
-      />
-      <Main
-        selectedTab={selectedTab}
-        MENU_TABS={MENU_TABS}
-        tasks={tasks}
-        updateTaskStatus={updateTaskStatus}
-        deleteTask={deleteTask}
-      />
-    </div>
-  );
+    function updateTaskStatus(taskId) {
+        setTasks(prevTasks => prevTasks.map(task => {
+            if (task.id === taskId) {
+                return {
+                  ...task,
+                  active: !task.active
+                }
+              } else {
+                return task
+            }
+        }))
+      }
+
+    function deleteTask(taskId) {
+        setTasks(prevTasks => prevTasks.filter(task => task.id !== taskId))
+    }
+
+    return (
+        <div className="app">
+            <Header
+                selectedTab={selectedTab}
+                MENU_TABS={MENU_TABS}
+                tasks={tasks}
+                handleTaskCreate={handleTaskCreate}
+                handleTabSelection={handleTabSelection}
+            />
+            <Main
+                selectedTab={selectedTab}
+                MENU_TABS={MENU_TABS}
+                tasks={tasks}
+                updateTaskStatus={updateTaskStatus}
+                deleteTask={deleteTask}
+            />
+        </div>
+    );
 }
 
 export default App;
