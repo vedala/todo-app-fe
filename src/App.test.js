05102cde517050c332d8137730439f153c4c68e8
Kalyan Vedala <svedala23@gmail.com>
2022-08-24 14:46:10 -0700

    Add an expect check

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,9 @@
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
 import App from './App'
 
 test('renders the App page', () => {
     render(<App />)
+
+    expect(screen.getByRole("heading")).toHaveTextContent(/To-Do List/)
 })
