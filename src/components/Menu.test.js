723a6c221886ea05036c3e74acc3e7d4cdc945d7
Kalyan Vedala <svedala23@gmail.com>
2022-08-29 12:08:17 -0700

    Setup a basic test for Menu component

---
+++ src/components/Menu.test.js
@@ -0,0 +1,40 @@
+import { render, screen } from '@testing-library/react'
+import Menu from './Menu'
+
+test('renders the menu', () => {
+    const tasks = [
+        {
+            id: 1,
+            text: "item-1",
+            active: true,
+        },
+        {
+            id: 2,
+            text: "item-2",
+            active: true,
+        },
+        {
+            id: 3,
+            text: "item-3",
+            active: false,
+        },
+        {
+            id: 4,
+            text: "item-4",
+            active: false,
+        },
+        {
+            id: 5,
+            text: "item-5",
+            active: true,
+        },
+    ]
+
+    const MENU_TABS = {
+        TO_DO: 'TO_DO',
+        DONE: 'DONE',
+        ALL: 'ALL'
+    }
+
+    render(<Menu tasks={tasks} MENU_TABS={MENU_TABS.TO_DO} />)
+})
