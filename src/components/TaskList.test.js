b54a87c96bc844bd767068464a3b696556265fee
Kalyan Vedala <svedala23@gmail.com>
2022-08-29 14:26:13 -0700

    Add test for ALL tab

--- src/components/TaskList.test.js
+++ src/components/TaskList.test.js
@@ -88,3 +88,50 @@ test('renders the task list for DONE tab', () => {
     expect(items[1]).toHaveTextContent("item-4")
 
 })
+
+
+test('renders the task list for ALL tab', () => {
+    const tasks = [
+        {
+            id: 1,
+            text: "item-1",
+            active: true,
+        },
+        {
+            id: 2,
+            text: "item-2",
+            active: true,
+        },
+        {
+            id: 3,
+            text: "item-3",
+            active: false,
+        },
+        {
+            id: 4,
+            text: "item-4",
+            active: false,
+        },
+        {
+            id: 5,
+            text: "item-5",
+            active: true,
+        },
+    ]
+
+    const MENU_TABS = {
+        TO_DO: 'TO_DO',
+        DONE: 'DONE',
+        ALL: 'ALL'
+    }
+
+    render(<TaskList tasks={tasks} MENU_TABS={MENU_TABS} selectedTab={MENU_TABS.ALL} />)
+    const items = screen.getAllByText(/item/)
+    expect(items.length).toBe(5)
+    expect(items[0]).toHaveTextContent("item-1")
+    expect(items[1]).toHaveTextContent("item-2")
+    expect(items[2]).toHaveTextContent("item-3")
+    expect(items[3]).toHaveTextContent("item-4")
+    expect(items[4]).toHaveTextContent("item-5")
+
+})
